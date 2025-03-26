e4d9b8b9f5edd425f73a438209ff12545a96c42b
Bodya <harasukb@gmail.com>
2025-03-26 13:37:00 +0300

    v0.011

--- src/main/java/org/example/Backend/TableStorageManager/BytesConverters/BytesConverterFactory.java
+++ src/main/java/org/example/Backend/TableStorageManager/BytesConverters/BytesConverterFactory.java
@@ -3,14 +3,16 @@ package org.example.Backend.TableStorageManager.BytesConverters;
 import org.example.Backend.Models.TypeData;
 
 public class BytesConverterFactory {
+
     public BytesConverters<?> getBytesConverters(TypeData typeData) {
         return switch (typeData){
             case INT -> new BytesIntegerConverters();
             case LONG -> new BytesLongConverters();
             case VARCHAR -> new BytesStringConverters();
             case BOOLEAN -> new BytesBooleanConverters();
+            case DOUBLE -> new BytesDoubleConverters();
+            case DATE -> new BytesDateConverters();
             case TABLE_METADATA -> new BytesMetaDataConverters();
-            default -> null;
         };
     }
 }
